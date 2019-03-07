33b2ff0cb437b883dd4b4ae9c4e42927e8ddbe81
piyush vishnoi <piyushvishnoipvsh123@gmail.com>
2019-03-07 10:07:37 +0530

    definitions of two functions

--- hello.c
+++ hello.c
@@ -3,12 +3,20 @@ using namespace std;
 class my
 {
 public:
-void mybranch1();
-void mybranch2();
+void mybranch1()
+{
+cout<<"my first function";
+}
+void mybranch2()
+{
+cout<<"my second function";
+}
 };
 int main()
 {
 my m;
+m.mybranch1();
+m.mybranch2();
 return 0;
 }
 
